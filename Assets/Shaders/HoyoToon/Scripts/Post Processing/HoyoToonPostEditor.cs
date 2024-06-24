05be7d8b261ff49e50f0ace7ef8565a5d39a7962
IamfkingNOOB <iamfkingnoob@gmail.com>
2024-06-24 10:29:24 +0900

    【Init】 Create Project
    1. 프로젝트를 생성합니다.
    2. 필요한 패키지들을 설치합니다.
    3. 'HoyoToon' Shader를 추가합니다.

---
+++ Assets/Shaders/HoyoToon/Scripts/Post Processing/HoyoToonPostEditor.cs
@@ -0,0 +1,79 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(HoyoToonPostProcess))]
+public class HoyoToonPostProcessEditor : Editor
+{
+    private bool showBloomSettings = true;
+    private bool showColorGrading = true;
+    private bool showToneMapping = true;
+
+
+    public override void OnInspectorGUI()
+    {
+        HoyoToonPostProcess script = (HoyoToonPostProcess)target;
+
+        // Record the target object before making changes
+        Undo.RecordObject(script, "Modify HoyoToonPostProcess");
+
+        // Load and draw logo;
+        Texture2D logo = Resources.Load<Texture2D>("UI/hoyotoon");
+        if (logo != null)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label(logo);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        // Bloom settings
+        showBloomSettings = EditorGUILayout.BeginFoldoutHeaderGroup(showBloomSettings, "Bloom Settings");
+        if (showBloomSettings)
+        {
+            EditorGUI.indentLevel++;
+
+            script.bloomMode = (HoyoToonPostProcess.BloomMode)EditorGUILayout.EnumPopup("Bloom Mode", script.bloomMode);
+            script.bloomThreshold = EditorGUILayout.FloatField("Bloom Threshold", script.bloomThreshold);
+            script.bloomIntensity = EditorGUILayout.FloatField("Bloom Intensity", script.bloomIntensity);
+            script.bloomWeights = EditorGUILayout.Vector4Field("Bloom Weights", script.bloomWeights);
+            script.bloomColor = EditorGUILayout.ColorField("Bloom Color", script.bloomColor);
+            script.blurSamples = EditorGUILayout.FloatField("Blur Samples", script.blurSamples);
+            script.blurWeight = EditorGUILayout.FloatField("Blur Weight", script.blurWeight);
+            script.downsampleValue = EditorGUILayout.Slider("Downsample Value", script.downsampleValue, 0.1f, 1.0f);
+
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        // Color Grading settings
+        showColorGrading = EditorGUILayout.BeginFoldoutHeaderGroup(showColorGrading, "Color Grading Settings");
+        if (showColorGrading)
+        {
+            script.toneMode = (HoyoToonPostProcess.ToneMode)EditorGUILayout.EnumPopup("Tone Mode", script.toneMode);
+            script.exposure = EditorGUILayout.FloatField("Exposure", script.exposure);
+            script.contrast = EditorGUILayout.Slider("Contrast", script.contrast, 0.0f, 2.0f);
+            script.saturation = EditorGUILayout.Slider("Saturation", script.saturation, 0.0f, 2.0f);
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        if (script.toneMode == HoyoToonPostProcess.ToneMode.StarRail)
+        {
+            // Tone Mapping settings
+            showToneMapping = EditorGUILayout.BeginFoldoutHeaderGroup(showToneMapping, "Tone Mapping Settings");
+            if (showToneMapping)
+            {
+                script.ACESParamA = EditorGUILayout.FloatField("ACES Param A", script.ACESParamA);
+                script.ACESParamB = EditorGUILayout.FloatField("ACES Param B", script.ACESParamB);
+                script.ACESParamC = EditorGUILayout.FloatField("ACES Param C", script.ACESParamC);
+                script.ACESParamD = EditorGUILayout.FloatField("ACES Param D", script.ACESParamD);
+                script.ACESParamE = EditorGUILayout.FloatField("ACES Param E", script.ACESParamE);
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+        }
+    }
+}
+#endif
---
+++ README.md
@@ -0,0 +1,2 @@
+# Kyeongil_Final
+ 
