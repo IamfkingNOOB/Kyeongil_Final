1a35949019c902a841609e6c2ed5417bf01210ef
IamfkingNOOB <iamfkingnoob@gmail.com>
2024-06-28 11:09:52 +0900

    【Feat】 메인 화면의 버튼을 UIToolkit으로 구성, 클릭 애니메이션 구현

--- Assets/Scripts/PlayerController_Fuka_C6.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.InputSystem;
-
-public class PlayerController_Fuka_C6 : BasePlayerController
-{
-    public override void OnWeapon(InputAction.CallbackContext callbackContext)
-    {
-        _animator.SetTrigger(_weaponAnim_ToHash);
-    }
-}
